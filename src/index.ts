69938033d301c7ba3693837791fcc715d8e33bd8
forinda <forinda82@gmail.com>
2024-05-17 13:11:11 +0300

    updatew queue logging

--- src/index.ts
+++ src/index.ts
@@ -20,12 +20,6 @@ const syncFn2 = () => {
 const syncFn3 = () => console.log("Sync Function 3");
 
 // Asynchronous functions
-const asyncFn1 = () =>
-  new Promise<void>((resolve) => {
-    throw new Error("Some error");
-    console.log("Async Function 1");
-    setTimeout(resolve, 1000);
-  });
 
 const asyncFn2 = () =>
   new Promise<void>((resolve) => {
@@ -38,17 +32,15 @@ const asyncFn3 = () =>
     console.log("Async Function 3");
     setTimeout(resolve, 700);
   });
-const viewQueue = () => console.log({ QUEUE: myQueue });
-
+// const viewQueue = () => console.log({ QUEUE: myQueue });
+const asyncFn1 = () =>
+  new Promise<void>(() => {
+    throw new Error("Some error");
+  });
 // Enqueue functions
 myQueue.enqueue(syncFn1);
-myQueue.enqueue(asyncFn1, { retry_times: 4 });
-myQueue.enqueue(syncFn2, { retry_times: 2 });
-myQueue.enqueue(viewQueue);
+myQueue.enqueue(asyncFn1, { retry_times: 5 });
+myQueue.enqueue(syncFn2, { retry_times: 7 });
 myQueue.enqueue(asyncFn2);
-myQueue.enqueue(viewQueue);
 myQueue.enqueue(syncFn3);
-myQueue.enqueue(viewQueue);
 myQueue.enqueue(asyncFn3);
-myQueue.enqueue(viewQueue);
-
